5291ff704a39b7b546955710ad70c13fbe8b1b2f
selqitz <101630390+selqitz@users.noreply.github.com>
2022-04-19 16:25:11 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ResponsiPemrogramann4265
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Karyawan karyawan = new Karyawan(19112712, "Slamet", 5000000);
+            Karyawan karyawan2 = new Karyawan(19112712, "Reza", 20000000);
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan.showKaryawan();
+            karyawan2.showKaryawan();
+
+            Console.WriteLine("\n\n Bahagianya Naik Gaji");
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class Karyawan
+    {
+        private int v1;
+        private string v2;
+        private int v3;
+
+        public Karyawan(int v1, string v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+    }
